f0829727f4d50d88500c613449b4f29af370a390
Shiv Parikh <shivparikh@Shivs-MacBook-Pro.local>
2017-04-04 14:37:22 -0700

    reformat playground into four sandboxes

---
+++ charts/top-left.js
@@ -0,0 +1,30 @@
+(function(){
+    var data = [4, 8, 15, 16, 23, 42];
+
+    var width = 420,
+        barHeight = 20;
+
+    // v4 uses scaleLinear instead of scale.linear
+    var x = d3.scaleLinear()
+        .domain([0, d3.max(data)])
+        .range([0, width]);
+
+    var chart = d3.select(".chart-top-left")
+        .attr("width", width)
+        .attr("height", barHeight * data.length);
+
+    var bar = chart.selectAll("g")
+        .data(data)
+      .enter().append("g")
+        .attr("transform", (d, i) => { return "translate(0," + i * barHeight + ")"; });
+
+    bar.append("rect")
+        .attr("width", x)
+        .attr("height", barHeight - 1);
+
+    bar.append("text")
+        .attr("x", (d) => { return x(d) - 3; })
+        .attr("y", barHeight / 2)
+        .attr("dy", ".35em")
+        .text((d) => { return d; });
+}());
--- charts/top-right.js
+++ charts/top-right.js
@@ -9,22 +9,22 @@
         .domain([0, d3.max(data)])
         .range([0, width]);
 
-    var chart = d3.select(".chart")
+    var chart = d3.select(".chart-top-right")
         .attr("width", width)
         .attr("height", barHeight * data.length);
 
     var bar = chart.selectAll("g")
         .data(data)
       .enter().append("g")
-        .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+        .attr("transform", (d, i) => { return "translate(0," + i * barHeight + ")"; });
 
     bar.append("rect")
         .attr("width", x)
         .attr("height", barHeight - 1);
 
     bar.append("text")
-        .attr("x", function(d) { return x(d) - 3; })
+        .attr("x", (d) => { return x(d) - 3; })
         .attr("y", barHeight / 2)
         .attr("dy", ".35em")
-        .text(function(d) { return d; });
+        .text((d) => { return d; });
 }());
